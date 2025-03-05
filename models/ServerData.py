84422c0240f2f04e0efa16e96dedd6f2142ada0e
Karla Feliciano <karla.s.feliciano@gmail.com>
2025-03-05 19:55:38 -0300

    change enum file

--- models/ServerData.py
+++ models/ServerData.py
@@ -1,5 +1,7 @@
 from datetime import datetime
-from pydantic import BaseModel
+from pydantic import BaseModel, model_validator
+from typing_extensions import Self
+from enum import Enum
 
 class ServerData(BaseModel):
     server_ulid: str
@@ -8,3 +10,15 @@ class ServerData(BaseModel):
     humidity: float | None = None
     voltage: float | None = None
     current: float | None = None
+
+    @model_validator(mode='after')
+    def check_at_least_one_measurement(self) -> Self:
+        if not any([self.temperature, self.humidity, self.voltage, self.current]):
+            raise ValueError('At least one sensor value must be provided')
+        return self
+
+class SensorType(str, Enum):
+    TEMPERATURE = "temperature"
+    HUMIDITY = "humidity"
+    VOLTAGE = "voltage"
+    CURRENT = "current"
